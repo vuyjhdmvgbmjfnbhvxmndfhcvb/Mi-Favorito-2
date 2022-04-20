eb00c9a5dd93d028ca791b1d66c1d0d468e2fd2b
discordia976 <94662366+vuyjhdmvgbmjfnbhvxmndfhcvb@users.noreply.github.com>
2022-04-19 18:38:46 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function popo(){
+document.getElementById("kakav420").src="https://c.tenor.com/wbXD-9vRLrQAAAAC/gotcha-got-you.gif"
+
+
+}
+function kaka(){
+document.getElementById("kakav420").src="https://c.tenor.com/wbXD-9vRLrQAAAAC/gotcha-got-you.gif"
+
+}
